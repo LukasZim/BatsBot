cea1377db52d88b7946a2874dc75ed5d15ae67e7
Lukas Zimmerhackl <lukas.zim@hotmail.com>
2020-05-03 02:36:51 +0200

    Update source.js

--- commands/source.js
+++ commands/source.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 module.exports.run = async(bot, message, args) => {
     console.log(packageStuff);
     console.log(packageStuff.homepage);
+    message.channel.send(packageStuff.homepage);
 }
 
 module.exports.help = {
